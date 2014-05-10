eb24b4c39fa1409aaced16c6b1e39b4f516ac42c
Grant <grant.bartel@gmail.com>
2014-05-10 19:46:37 +0200

    Sequential code working

--- 03_assignment/gol_seq.c
+++ 03_assignment/gol_seq.c
@@ -3,7 +3,133 @@
 
 void evolve(unsigned char *grid_in, unsigned char *grid_out, unsigned int dim_x, unsigned int dim_y, unsigned int x, unsigned int y)
 {
+	unsigned char (*c_grid_in)[dim_x] = (unsigned char (*)[dim_x])grid_in;
+	unsigned char (*c_grid_out)[dim_x] = (unsigned char (*)[dim_x])grid_out;
+	unsigned int aliveNeighborCnt = 0;
 
+	// Avoid references outside of the grid
+	if(x > 0 && x < (dim_x - 1) && y > 0 && y < (dim_y - 1))  // Inner cells
+	{
+		if(c_grid_in[y][x - 1] == 1) 		{ aliveNeighborCnt++; }
+		if(c_grid_in[y][x + 1] == 1) 		{ aliveNeighborCnt++; }
+		if(c_grid_in[y - 1][x] == 1)		{ aliveNeighborCnt++; }
+		if(c_grid_in[y + 1][x] == 1)		{ aliveNeighborCnt++; }
+
+		if(c_grid_in[y - 1][x - 1] == 1) 	{ aliveNeighborCnt++; }
+		if(c_grid_in[y - 1][x + 1] == 1) 	{ aliveNeighborCnt++; }
+		if(c_grid_in[y + 1][x - 1] == 1)	{ aliveNeighborCnt++; }
+		if(c_grid_in[y + 1][x + 1] == 1)	{ aliveNeighborCnt++; }
+	}
+	else if(x > 0 && x < (dim_x - 1) && y == 0)  // Bottom border cells //
+	{
+		if(c_grid_in[y][x - 1] == 1) 			{ aliveNeighborCnt++; }
+		if(c_grid_in[y][x + 1] == 1) 			{ aliveNeighborCnt++; }
+		if(c_grid_in[dim_y - 1][x] == 1)		{ aliveNeighborCnt++; } //
+		if(c_grid_in[y + 1][x] == 1)			{ aliveNeighborCnt++; }
+
+		if(c_grid_in[dim_y - 1][x - 1] == 1) 	{ aliveNeighborCnt++; } //
+		if(c_grid_in[dim_y - 1][x + 1] == 1) 	{ aliveNeighborCnt++; } //
+		if(c_grid_in[y + 1][x - 1] == 1)		{ aliveNeighborCnt++; }
+		if(c_grid_in[y + 1][x + 1] == 1)		{ aliveNeighborCnt++; }
+	}
+	else if(x > 0 && x < (dim_x - 1) && y == (dim_y - 1))  // Top border cells //
+	{
+		if(c_grid_in[y][x - 1] == 1) 			{ aliveNeighborCnt++; }
+		if(c_grid_in[y][x + 1] == 1) 			{ aliveNeighborCnt++; }
+		if(c_grid_in[y - 1][x] == 1)			{ aliveNeighborCnt++; }
+		if(c_grid_in[0][x] == 1)				{ aliveNeighborCnt++; } //
+
+		if(c_grid_in[y - 1][x - 1] == 1) 		{ aliveNeighborCnt++; }
+		if(c_grid_in[y - 1][x + 1] == 1) 		{ aliveNeighborCnt++; }
+		if(c_grid_in[0][x - 1] == 1)			{ aliveNeighborCnt++; } //
+		if(c_grid_in[0][x + 1] == 1)			{ aliveNeighborCnt++; } //
+	}
+	else if(x == 0 && y > 0 && y < (dim_y - 1))  // Left border cells //
+	{
+		if(c_grid_in[y][dim_x - 1] == 1) 		{ aliveNeighborCnt++; } //
+		if(c_grid_in[y][x + 1] == 1) 			{ aliveNeighborCnt++; }
+		if(c_grid_in[y - 1][x] == 1)			{ aliveNeighborCnt++; }
+		if(c_grid_in[y + 1][x] == 1)			{ aliveNeighborCnt++; }
+
+		if(c_grid_in[y - 1][dim_x - 1] == 1) 	{ aliveNeighborCnt++; } //
+		if(c_grid_in[y - 1][x + 1] == 1) 		{ aliveNeighborCnt++; }
+		if(c_grid_in[y + 1][dim_x - 1] == 1)	{ aliveNeighborCnt++; } //
+		if(c_grid_in[y + 1][x + 1] == 1)		{ aliveNeighborCnt++; }
+	}
+	else if(x == (dim_x - 1) && y > 0 && y < (dim_y - 1))  // Right border cells //
+	{
+		if(c_grid_in[y][x - 1] == 1) 			{ aliveNeighborCnt++; }
+		if(c_grid_in[y][0] == 1) 				{ aliveNeighborCnt++; } //
+		if(c_grid_in[y - 1][x] == 1)			{ aliveNeighborCnt++; }
+		if(c_grid_in[y + 1][x] == 1)			{ aliveNeighborCnt++; }
+
+		if(c_grid_in[y - 1][x - 1] == 1) 		{ aliveNeighborCnt++; }
+		if(c_grid_in[y - 1][0] == 1) 			{ aliveNeighborCnt++; } //
+		if(c_grid_in[y + 1][x - 1] == 1)		{ aliveNeighborCnt++; }
+		if(c_grid_in[y + 1][0] == 1)			{ aliveNeighborCnt++; } //
+	}
+	else if(x == 0 && y == 0)  // Bottom left corner cell //
+	{
+		if(c_grid_in[y][dim_x - 1] == 1) 		{ aliveNeighborCnt++; } //
+		if(c_grid_in[y][x + 1] == 1) 			{ aliveNeighborCnt++; }
+		if(c_grid_in[dim_y - 1][x] == 1)		{ aliveNeighborCnt++; } //
+		if(c_grid_in[y + 1][x] == 1)			{ aliveNeighborCnt++; }
+
+		if(c_grid_in[dim_y - 1][dim_x - 1] == 1){ aliveNeighborCnt++; } //
+		if(c_grid_in[dim_y - 1][x + 1] == 1) 	{ aliveNeighborCnt++; } //
+		if(c_grid_in[y + 1][dim_x - 1] == 1)	{ aliveNeighborCnt++; } //
+		if(c_grid_in[y + 1][x + 1] == 1)		{ aliveNeighborCnt++; }
+	}
+	else if(x == 0 && y == (dim_y - 1))  // Top left corner cell //
+	{
+		if(c_grid_in[y][dim_x - 1] == 1) 		{ aliveNeighborCnt++; } //
+		if(c_grid_in[y][x + 1] == 1) 			{ aliveNeighborCnt++; }
+		if(c_grid_in[y - 1][x] == 1)			{ aliveNeighborCnt++; }
+		if(c_grid_in[0][x] == 1)				{ aliveNeighborCnt++; } //
+
+		if(c_grid_in[y - 1][dim_x - 1] == 1) 	{ aliveNeighborCnt++; } //
+		if(c_grid_in[y - 1][x + 1] == 1) 		{ aliveNeighborCnt++; }
+		if(c_grid_in[0][dim_x - 1] == 1)		{ aliveNeighborCnt++; } //
+		if(c_grid_in[0][x + 1] == 1)			{ aliveNeighborCnt++; } //
+	}
+	else if(x == (dim_x - 1) && y == 0)  // Bottom right corner cell //
+	{
+		if(c_grid_in[y][x - 1] == 1) 			{ aliveNeighborCnt++; }
+		if(c_grid_in[y][0] == 1)	 			{ aliveNeighborCnt++; } //
+		if(c_grid_in[dim_y - 1][x] == 1)		{ aliveNeighborCnt++; } //
+		if(c_grid_in[y + 1][x] == 1)			{ aliveNeighborCnt++; }
+
+		if(c_grid_in[dim_y - 1][x - 1] == 1)	{ aliveNeighborCnt++; } //
+		if(c_grid_in[dim_y - 1][0] == 1) 		{ aliveNeighborCnt++; } //
+		if(c_grid_in[y + 1][x - 1] == 1)		{ aliveNeighborCnt++; }
+		if(c_grid_in[y + 1][0] == 1)			{ aliveNeighborCnt++; } //
+	}
+	else if(x == (dim_x - 1) && y == (dim_y - 1))  // Top right corner cell //
+	{
+		if(c_grid_in[y][x - 1] == 1) 			{ aliveNeighborCnt++; }
+		if(c_grid_in[y][0] == 1) 				{ aliveNeighborCnt++; } //
+		if(c_grid_in[y - 1][x] == 1)			{ aliveNeighborCnt++; }
+		if(c_grid_in[0][x] == 1)				{ aliveNeighborCnt++; } //
+
+		if(c_grid_in[y - 1][x - 1] == 1) 		{ aliveNeighborCnt++; }
+		if(c_grid_in[y - 1][0] == 1) 			{ aliveNeighborCnt++; } //
+		if(c_grid_in[0][x - 1] == 1)			{ aliveNeighborCnt++; } //
+		if(c_grid_in[0][0] == 1)				{ aliveNeighborCnt++; } //
+	}
+
+	// Determine new cell state
+	if(c_grid_in[y][x] == 1 && (aliveNeighborCnt < 2 || aliveNeighborCnt > 3))
+	{
+		c_grid_out[y][x] = 0;  	// Kill cell
+	}
+	else if(c_grid_in[y][x] == 0 && aliveNeighborCnt == 3)
+	{
+		c_grid_out[y][x] = 1;  	// Create cell
+	}
+	else
+	{
+		c_grid_out[y][x] = c_grid_in[y][x];
+	}
 }
 
 void swap(unsigned char **a, unsigned char **b)
