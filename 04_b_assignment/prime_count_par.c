482c8f664b03c354579828a36c0575640f8b3101
Grant <grant.bartel@gmail.com>
2014-05-26 23:01:59 +0200

    Simplified prime count

--- 04_b_assignment/prime_count_par.c
+++ 04_b_assignment/prime_count_par.c
@@ -1,29 +1,28 @@
 #include <stdlib.h>
 #include <math.h>
-#include <stdbool.h>
 #include <omp.h>
 
-bool IsPrime(unsigned long test_num)
+int IsPrime(unsigned long test_num)
 {
-	bool is_prime;
+	int is_prime;
 
 	if(test_num != 1)
 	{
-		is_prime = true;
+		is_prime = 1;
 		float k_max = sqrtf(test_num);
 
 		for(unsigned long k = 2; k <= k_max; k++)
 		{
 			if(test_num % k == 0)
 			{
-				is_prime = false;
+				is_prime = 0;
 				break;
 			}
 		}
 	}
 	else
 	{
-		is_prime = false;
+		is_prime = 0;
 	}
 
 	return is_prime;
@@ -32,7 +31,6 @@ bool IsPrime(unsigned long test_num)
 unsigned long prime_count(unsigned long a, unsigned long b, unsigned long num_threads, unsigned long chunk_size)
 {
 	unsigned long prime_cnt = 0;
-	bool test_return = false;
 
 	// Don't evaluate zero
 	if(a == 0)
@@ -44,15 +42,10 @@ unsigned long prime_count(unsigned long a, unsigned long b, unsigned long num_th
 	omp_set_num_threads(num_threads);
 
 	// Iterate through provided range
-	#pragma omp parallel for schedule(dynamic, chunk_size) private(test_return) reduction(+: prime_cnt)
+	#pragma omp parallel for schedule(dynamic, chunk_size) reduction(+: prime_cnt)
 	for(unsigned long i = a; i <= b; i++)
 	{
-		test_return = IsPrime(i);
-
-		if(test_return == true)
-		{
-			prime_cnt++;
-		}
+		prime_cnt += IsPrime(i);
 	}
 
 	return prime_cnt;
