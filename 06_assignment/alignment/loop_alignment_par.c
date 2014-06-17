f9363152a07d7b120406884c0304e815f608f6a9
Grant <grant.bartel@gmail.com>
2014-06-17 20:01:51 +0200

    Finished alignment

--- 06_assignment/alignment/loop_alignment_par.c
+++ 06_assignment/alignment/loop_alignment_par.c
@@ -1,11 +1,23 @@
+#include <omp.h>
 
-void compute(unsigned long **a, unsigned long **b, unsigned long **c, unsigned long **d, int N) {
+void compute(unsigned long **a, unsigned long **b, unsigned long **c, unsigned long **d, int N)
+{
+	int i, j;
 
-	for (int i=1; i<N; i++) {
-		for (int j=1; j<N; j++) {
-			a[i][j] = 3*b[i][j];
-			b[i][j+1] = c[i][j]*c[i][j];
-			c[i][j-1] = a[i][j] * d[i][j];
+	#pragma omp parallel for
+	for(i=1; i<N; i++)
+	{
+		a[i][1] = 3*b[i][1];				// S1
+		c[i][0] = a[i][1] * d[i][1];		// S3
+
+		#pragma omp parallel for
+		for(j=2; j<N; j++)
+		{
+			b[i][j] = c[i][j-1]*c[i][j-1];	// S2
+			a[i][j] = 3*b[i][j];			// S1
+			c[i][j-1] = a[i][j] * d[i][j];	// S3
 		}
+
+		b[i][N] = c[i][N-1]*c[i][N-1];		// S2
 	}
 }
