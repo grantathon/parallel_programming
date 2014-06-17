2afc1472ecd89a40b75b4822598512b5d80ef1a8
Grant <grant.bartel@gmail.com>
2014-06-17 17:04:29 +0200

    Finished fission

--- 06_assignment/fission/loop_fission_par.c
+++ 06_assignment/fission/loop_fission_par.c
@@ -4,64 +4,19 @@ void compute(unsigned long **a, unsigned long **b, unsigned long **c, unsigned l
 {
 	int j, i;
 
-//	#pragma omp parallel for private (i)
-//	for (j=1; j<N; j++)
-//	{
-//		for (i=1; i<N; i++)
-//		{
-////			a[i][j]   = a[i][j]*b[i][j];  		// S1
-//
-////			b[i][j+1] = 2*a[i][j]*c[i-1][j];	// S2
-//
-//			c[i][j]   = 3*d[i][j];				// S3
-//
-////			d[i][j]   = 2*c[i+1][j];			// S4
-//		}
-//
-//		for (i=1; i<N; i++)
-//		{
-//			d[i][j]   = 2*c[i+1][j];			// S4
-//		}
-//
-////		for (i=1; i<N; i++)
-////		{
-////			b[i][j+1] = 2*a[i][j]*c[i-1][j];	// S2
-////		}
-//	}
-
-	#pragma omp parallel for private(i)
-	for (j=1; j<N; j++)
-	{
-		for (i=1; i<N; i++)
-		{
-			a[i][j]   = a[i][j]*b[i][j];  		// S1
-		}
-	}
-
-	#pragma omp parallel for private(i)
-	for (j=1; j<N; j++)
-	{
-		for (i=1; i<N; i++)
-		{
-			b[i][j+1] = 2*a[i][j]*c[i-1][j];	// S2
-		}
-	}
-
-	#pragma omp parallel for private(i)
 	for (j=1; j<N; j++)
 	{
 		for (i=1; i<N; i++)
 		{
 			c[i][j]   = 3*d[i][j];				// S3
+			d[i][j]   = 2*c[i+1][j];			// S4
 		}
-	}
 
-	#pragma omp parallel for private(i)
-	for (j=1; j<N; j++)
-	{
+		#pragma omp parallel for
 		for (i=1; i<N; i++)
 		{
-			d[i][j]   = 2*c[i+1][j];			// S4
+			a[i][j]   = a[i][j]*b[i][j];  		// S1
+			b[i][j+1] = 2*a[i][j]*c[i-1][j];	// S2
 		}
 	}
 }
