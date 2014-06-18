e8ef31707ee2abc2f88a8bc3eb7be56fb413733d
Grant <grant.bartel@gmail.com>
2014-06-18 17:12:36 +0200

    Finished fusion

--- 06_assignment/fusion/loop_fusion_par.c
+++ 06_assignment/fusion/loop_fusion_par.c
@@ -1,15 +1,21 @@
-void compute(unsigned long **a, unsigned long **b, unsigned long **c, unsigned long **d, int N) {
+#include <omp.h>
 
-	for (int i=1; i<N; i++) {
-		for (int j=1; j<N; j++) {
-			a[i][j] = 2 * b[i][j];
-			d[i][j] = a[i][j] * c[i][j];
+void compute(unsigned long **a, unsigned long **b, unsigned long **c, unsigned long **d, int N)
+{
+	int i, j;
+
+	for(i=1; i<N; i++)
+	{
+		#pragma omp parallel for
+		for(j=1; j<N; j++)
+		{
+			a[i][j] = 2 * b[i][j];				// S1
+			d[i][j] = a[i][j] * c[i][j];		// S2
 		}
-	}
 
-	for (int j=1; j<N; j++) {
-		for (int i=1; i<N; i++) {
-			c[i][j-1] = a[i][j-1] - a[i][j+1];
+		for(j=1; j<N; j++)
+		{
+			c[i][j-1] = a[i][j-1] - a[i][j+1];	// S3
 		}
 	}
 }
